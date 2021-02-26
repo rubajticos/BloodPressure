cecec907d67c2a22618f416f5fca4c135a1e4761
Michal Rubajczyk <rubajticos@gmail.com>
2021-02-26 19:57:08 +0100

    Create Diary module stub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiaryComponent } from './component/diary/diary/diary.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: DiaryComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { DiaryComponent } from './component/diary/diary/diary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DiaryComponent
   ],
   imports: [
     BrowserModule,
