ccf679bc01535e5dc38ee54859d539bb669dafbf
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-02 22:32:56 +0100

    Add missing file

---
+++ src/app/component/diary/diary-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DiaryComponent } from './diary/diary.component';
+
+const routes: Routes = [{ path: '', component: DiaryComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class DiaryRoutingModule {}
