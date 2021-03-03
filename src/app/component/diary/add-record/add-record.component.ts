42eabca2333ea3e5dccded47126e8bbdd4605803
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-03 20:04:36 +0100

    Fix compilation

--- src/app/component/diary/add-record/add-record.component.ts
+++ src/app/component/diary/add-record/add-record.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { DiaryService } from '../diary.service';
 
 @Component({
   selector: 'app-add-record',
   templateUrl: './add-record.component.html',
-  styleUrls: ['./add-record.component.sass']
+  styleUrls: ['./add-record.component.sass'],
 })
 export class AddRecordComponent implements OnInit {
-
-  constructor() { }
+  constructor(private diaryService: DiaryService) {}
 
   ngOnInit(): void {
+    this.diaryService;
   }
-
 }
