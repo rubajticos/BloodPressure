5bd101e29d8638041fc73d61d02ec2d75c0ca961
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-07 21:33:43 +0100

    Configure route to display records list

--- src/app/component/diary/diary-routing.module.ts
+++ src/app/component/diary/diary-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiaryComponent } from './diary/diary.component';
+import { RecordsComponent } from './records/records.component';
 
-const routes: Routes = [{ path: '', component: DiaryComponent }];
+const routes: Routes = [{ path: '', component: DiaryComponent, children: [
+  {path: '', component: RecordsComponent}
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
