c79f4f20860c2b16f7686273c50ccf34743a31ea
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-07 21:55:02 +0100

    Subscribe records list and display

--- src/app/component/diary/records/records.component.ts
+++ src/app/component/diary/records/records.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DiaryRecord } from '../diary-record';
+import { DiaryService } from '../diary.service';
 
 @Component({
   selector: 'app-records',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./records.component.scss']
 })
 export class RecordsComponent implements OnInit {
+  diaryRecords: DiaryRecord[] = [];
 
-  constructor() { }
+  constructor(private diaryService: DiaryService) { }
 
   ngOnInit(): void {
+
+    this.diaryService.diaryRecordsChanged.subscribe(records => {
+      this.diaryRecords = records;
+    })
   }
 
 }
