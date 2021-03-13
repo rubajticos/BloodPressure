7b6eaf86b4224eebb890ff8dd1c0e196d0d2b51b
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-13 18:25:35 +0100

    Create component to display diary chart

--- src/app/component/diary/diary.module.ts
+++ src/app/component/diary/diary.module.ts
@@ -7,9 +7,10 @@ import { DiaryRoutingModule } from './diary-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RecordComponent } from './record/record.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
-  declarations: [DiaryComponent, AddRecordComponent, RecordsComponent, RecordComponent],
+  declarations: [DiaryComponent, AddRecordComponent, RecordsComponent, RecordComponent, ChartComponent],
   imports: [CommonModule, DiaryRoutingModule, ReactiveFormsModule, RouterModule],
   exports: [DiaryComponent, AddRecordComponent, RecordsComponent],
 })
