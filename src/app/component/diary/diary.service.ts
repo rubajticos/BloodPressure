8ebff460cdaefd1b1439e933ad7680e98b4acc54
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-13 15:49:19 +0100

    Add some temporary default records

--- src/app/component/diary/diary.service.ts
+++ src/app/component/diary/diary.service.ts
@@ -6,10 +6,19 @@ import { DiaryRecord } from './diary-record';
   providedIn: 'root',
 })
 export class DiaryService {
-  diaryRecordsChanged = new BehaviorSubject<DiaryRecord[]>([]);
-  private diaryRecords: DiaryRecord[] = [];
+  private defaultRecords = [
+    new DiaryRecord(120, 80, 60, "Fusce in semper dui.", new Date()),
+    new DiaryRecord(145, 65, 45, "Quisque posuere eu mauris at aliquam.", new Date()),
+    new DiaryRecord(200, 98, 78, "Fusce posuere pellentesque nulla vitae hendrerit.", new Date()),
+    new DiaryRecord(160, 120, 100, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis nunc dignissim, sodales risus id, ornare magna. Vivamus aliquam vel est non hendrerit. Nulla bibendum, massa in vestibulum facilisis, felis tortor placerat lectus, id lacinia risus metus sit amet felis. Proin sit amet nisl tristique, hendrerit arcu in, volutpat ipsum. Aenean ac fermentum lectus, a commodo nisi. ", new Date()),
+  ]
 
-  constructor() {}
+  diaryRecordsChanged = new BehaviorSubject<DiaryRecord[]>([]);
+  private diaryRecords: DiaryRecord[] = this.defaultRecords;
+
+  constructor() {
+    this.diaryRecordsChanged.next(this.diaryRecords.slice());
+  }
 
   public getRecords() {
     return this.diaryRecords.slice();
