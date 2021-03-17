533ba6d300a673026cf14aed3a464b68e61d99ee
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-17 23:51:43 +0100

    Add Firebase config to environments file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA8AzvW69fkxhh31xl5znqZ_Ffo53xJ5wk',
+    authDomain: 'blood-pressure-diary-b9c95.firebaseapp.com',
+    projectId: 'blood-pressure-diary-b9c95',
+    storageBucket: 'blood-pressure-diary-b9c95.appspot.com',
+    messagingSenderId: '747993810755',
+    appId: '1:747993810755:web:b2eee0f4f5e5f35ca611f6',
+    measurementId: 'G-Q0KF5CE47E',
+  },
 };
 
 /*
