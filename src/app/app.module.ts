c1a29f5d21842e984e1ea085f49a2fa97defc36d
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-18 00:21:53 +0100

    Init Firebase project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA8AzvW69fkxhh31xl5znqZ_Ffo53xJ5wk',
+    authDomain: 'blood-pressure-diary-b9c95.firebaseapp.com',
+    projectId: 'blood-pressure-diary-b9c95',
+    storageBucket: 'blood-pressure-diary-b9c95.appspot.com',
+    messagingSenderId: '747993810755',
+    appId: '1:747993810755:web:b2eee0f4f5e5f35ca611f6',
+    measurementId: 'G-Q0KF5CE47E',
+  },
 };
