87c728ce25e4640da7180aa097162119c2273422
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-18 23:20:01 +0100

    Add login/register component

--- src/app/component/auth/auth.module.ts
+++ src/app/component/auth/auth.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginRegisterComponent } from './loginregister/login-register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [{ path: '', component: LoginRegisterComponent  }];
-
+const routes: Routes = [{ path: '', component: LoginRegisterComponent }];
 
 @NgModule({
   declarations: [LoginRegisterComponent],
-  imports: [
-    CommonModule, RouterModule.forChild(routes)
-  ],
-  exports: [LoginRegisterComponent]
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
+  exports: [LoginRegisterComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/component/auth/loginregister/login-register.component.ts
+++ src/app/component/auth/loginregister/login-register.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-loginregister',
   templateUrl: './login-register.component.html',
-  styleUrls: ['./login-register.component.scss']
+  styleUrls: ['./login-register.component.scss'],
 })
 export class LoginRegisterComponent implements OnInit {
+  registerMode = false;
+  authForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getTitle() {
+    return this.registerMode ? 'Rejestracja' : 'Logowanie';
   }
 
+  getSubmitText() {
+    return this.registerMode ? 'Zarejestruj się' : 'Zaloguj się';
+  }
+
+  getToggleModeText() {
+    return this.registerMode
+      ? 'Przełącz na logowanie'
+      : 'Przełącz na rejestrację';
+  }
+
+  toggleMode() {
+    this.registerMode = !this.registerMode;
+  }
 }
