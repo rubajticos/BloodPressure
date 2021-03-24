0828545bc1347495a6f8e6208bce8138c3015d37
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-24 22:02:18 +0100

    Create Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './component/auth/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: '/diary', pathMatch: 'full' },
@@ -7,11 +8,13 @@ const routes: Routes = [
     path: 'diary',
     loadChildren: () =>
       import('./component/diary/diary.module').then((m) => m.DiaryModule),
+    canActivate: [AuthGuardService],
   },
   {
     path: 'auth',
-    loadChildren: () => import('./component/auth/auth.module').then((m) => m.AuthModule)
-  }
+    loadChildren: () =>
+      import('./component/auth/auth.module').then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
---
+++ src/app/component/auth/auth-guard.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardService implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.authState.pipe(
+      map((state) => {
+        if (state !== null) {
+          return true;
+        }
+
+        this.router.navigate(['auth']);
+        return false;
+      })
+    );
+  }
+}
