ce10d1d606c1018cc42ad7f5f471588fab55fb3a
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-24 23:00:48 +0100

    Manage nav links visibility depends on auth state

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isNavbarCollapsed = true;
+  authStateSubscription: Subscription;
 
-  constructor() {}
+  isAuthorized = false;
 
-  ngOnInit(): void {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authStateSubscription = this.authService.authState.subscribe({
+      next: (state) => {
+        if (state !== null) {
+          this.isAuthorized = true;
+        } else {
+          this.isAuthorized = false;
+        }
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authStateSubscription.unsubscribe();
+  }
 }
