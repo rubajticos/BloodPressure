255abea6f81dbedbf9c7a694676186dfb822bdc7
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-24 21:55:55 +0100

    Add user management methods to AuthService

--- src/app/component/auth/auth.service.ts
+++ src/app/component/auth/auth.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  readonly authState: Observable<firebase.User | null> = this.auth.authState;
+
   constructor(private auth: AngularFireAuth) {}
+
+  get user(): Promise<firebase.User | null> {
+    return this.auth.currentUser;
+  }
 
   registerNewUser(email: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(email, password);
@@ -13,5 +21,9 @@ export class AuthService {
 
   login(email: string, password: string) {
     return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
   }
 }
