a52f9ae9c070f37c895661334b3252a99d969e95
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-25 22:38:22 +0100

    Remove default records

--- src/app/component/diary/diary.service.ts
+++ src/app/component/diary/diary.service.ts
@@ -6,15 +6,8 @@ import { DiaryRecord } from './diary-record';
   providedIn: 'root',
 })
 export class DiaryService {
-  private defaultRecords = [
-    new DiaryRecord(120, 80, 60, "Fusce in semper dui.", new Date()),
-    new DiaryRecord(145, 65, 45, "Quisque posuere eu mauris at aliquam.", new Date()),
-    new DiaryRecord(200, 98, 78, "Fusce posuere pellentesque nulla vitae hendrerit.", new Date()),
-    new DiaryRecord(160, 120, 100, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis nunc dignissim, sodales risus id, ornare magna. Vivamus aliquam vel est non hendrerit. Nulla bibendum, massa in vestibulum facilisis, felis tortor placerat lectus, id lacinia risus metus sit amet felis. Proin sit amet nisl tristique, hendrerit arcu in, volutpat ipsum. Aenean ac fermentum lectus, a commodo nisi. ", new Date()),
-  ]
-
   diaryRecordsChanged = new BehaviorSubject<DiaryRecord[]>([]);
-  private diaryRecords: DiaryRecord[] = this.defaultRecords;
+  private diaryRecords: DiaryRecord[] = [];
 
   constructor() {
     this.diaryRecordsChanged.next(this.getRecords());
