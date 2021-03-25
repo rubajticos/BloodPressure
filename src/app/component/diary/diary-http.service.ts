1f921b7c33b98450ff643b47222bdfcfbcec2314
Michal Rubajczyk <rubajticos@gmail.com>
2021-03-25 22:57:31 +0100

    Build individual user link to store diary records

--- src/app/component/diary/diary-http.service.ts
+++ src/app/component/diary/diary-http.service.ts
@@ -1,17 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth/auth.service';
 import { DiaryRecord } from './diary-record';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DiaryHttpService {
-  private diaryUrl = environment.baseUrl + '/diary.json';
+  private userDiaryUrl: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthService) {
+    this.auth.user.then((user) => {
+      if (user !== null) {
+        this.userDiaryUrl =
+          environment.baseUrl + '/diary/' + user.uid + '.json';
+        console.log(this.userDiaryUrl);
+      }
+    });
+  }
 
   insertRecord(record: DiaryRecord) {
-    this.http.post(this.diaryUrl, record).subscribe((res) => console.log(res));
+    if (this.userDiaryUrl) {
+      this.http
+        .post(this.userDiaryUrl, record)
+        .subscribe((res) => console.log(res));
+    }
   }
 }
